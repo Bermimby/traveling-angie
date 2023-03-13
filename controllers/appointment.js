69258ed63f530a710dbe4ac1389463c138332b89
Bermimby <bermaimbert@hotmail.com>
2023-03-13 12:45:45 -0400

    index functionality

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -1,1 +1,15 @@
-const Appointment = require
+const Appointment = require('../../models/appointment')
+
+module.exports = {
+    index,
+}
+
+
+
+async function index(req, res) {
+    let appointment = null
+    if(req.user._id){
+      appointment = await Appointment.find({user : req.user._id})
+    }
+    res.json(appointment)
+  }
